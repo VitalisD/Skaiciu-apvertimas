d59e2b5e85d8bc2752f82001b9d1d06fbb97e97d
VitalisD <vitalis.dauparas@gmail.com>
2022-07-04 02:31:11 +0300

    Pridetas triju skaiciu palyginimas isvedimas i console

--- palyginimas.js
+++ palyginimas.js
@@ -6,8 +6,26 @@ if (palyginimui1 > palyginimui2) {
     //document.querySelector('.palyginimo-rezultatas').'#palyginimui1';
     //document.querySelector('.palyginimo-rezultatas').innerHTML = (palyginimui1);
     console.log (palyginimui1, '>')
-} else {
+} 
+else if (palyginimui1 == palyginimui2) {
+    console.log ('=')
+}
+else {
     //document.querySelector('.palyginimo-rezultatas').'#palyginimui2';
     //document.querySelector('.palyginimo-rezultatas').innerHTML = (palyginimui2);
     console.log ('<', palyginimui2)
 }
+
+let palyginimui3
+let palyginimui4
+let palyginimui5
+palyginimui3 = parseFloat(document.querySelector('#palyginimui3').value) ;
+palyginimui4 = parseFloat(document.querySelector('#palyginimui4').value) ;
+palyginimui5 = parseFloat(document.querySelector('#palyginimui5').value) ;
+if (palyginimui3 >= palyginimui4 && palyginimui3 >= palyginimui5) {
+    console.log (palyginimui3, '>')
+} else if (palyginimui5 >= palyginimui4 && palyginimui5 >= palyginimui3) {
+    console.log (palyginimui5, '>')
+} else {
+    console.log (palyginimui4, '>')
+    }
